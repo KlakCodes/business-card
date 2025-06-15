cda95d4481d411fdd09d0a502bb4d8903876f099
KlakCodes <62332729+KlakCodes@users.noreply.github.com>
2025-06-15 11:04:09 +0100

    Initial commit - webpack setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # business-card
+
+This is a site designed purely to be a virtual business card for my GitHub.
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+import { greeting } from "./greeting.js";
+import profileImage from "./assets/images/profile-pic.png"
+import profileGif from "./assets/images/profile-animated.gif"
+
+console.log(greeting);
+
+const image = document.createElement('img');
+image.src = profileImage;
+image.style.width = '300px'
+image.style.borderRadius = '300px'
+
+const gif = document.createElement('img');
+gif.src = profileGif;
+gif.style.width = '300px';
+gif.style.borderRadius = '300px';
+
+document.body.appendChild(image);
+document.body.appendChild(gif);
