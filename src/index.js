6c7f2be956e88cac8d57c3dfea67c701f3e68a2b
KlakCodes <62332729+KlakCodes@users.noreply.github.com>
2025-06-17 20:57:55 +0100

    Adjusted the image loading
    Images are now added in JavaScript and not in the HTML

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 import "./styles.css";
 import { greeting } from "./greeting.js";
 import profileImage from "./assets/images/profile-pic.png"
-import profileGif from "./assets/images/profile-animated.gif"
+import githubIcon from "./assets/images/github-mark.svg"
 
 console.log(greeting);
 
-// Example code
+const cardImage = document.querySelector('.card-image');
+cardImage.src = profileImage;
 
-// const image = document.createElement('img');
-// image.src = profileImage;
-// image.style.width = '300px'
-// image.style.borderRadius = '300px'
-
-// const gif = document.createElement('img');
-// gif.src = profileGif;
-// gif.style.width = '300px';
-// gif.style.borderRadius = '300px';
-
-// document.body.appendChild(image);
-// document.body.appendChild(gif);
+const linkIcon = document.querySelector('.link-icon');
+linkIcon.src = githubIcon;
 
 const footer = document.querySelector('.footer');
 const currentYear = new Date().getFullYear();
